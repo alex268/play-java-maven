8db1aa3ab5d6d42119e408303bb15cc1bf384eab
Alexandr Gorshenin <alex_gorshenin@mail.ru>
2022-09-26 16:03:41 +0300

    Added init upsert

--- app/services/YdbDataSource.java
+++ app/services/YdbDataSource.java
@@ -1,5 +1,14 @@
 package services;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+import javax.inject.Singleton;
+
 import com.yandex.ydb.core.Status;
 import com.yandex.ydb.core.UnexpectedResultException;
 import com.yandex.ydb.core.grpc.GrpcTransport;
@@ -13,13 +22,6 @@ import com.yandex.ydb.table.rpc.grpc.GrpcTableRpc;
 import com.yandex.ydb.table.transaction.TxControl;
 import com.yandex.ydb.table.values.PrimitiveType;
 import com.yandex.ydb.table.values.PrimitiveValue;
-import java.time.Duration;
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.UUID;
-import javax.inject.Singleton;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -41,21 +43,21 @@ public class YdbDataSource implements AutoCloseable, DataSource {
 
     private final TableClient client;
     private final SessionRetryContext retryCtx;
-    
+
     public YdbDataSource() {
         GrpcTransport transport = GrpcTransport.forEndpoint("localhost:2136", "local")
                 .withReadTimeout(Duration.ofSeconds(10))
                 .build();
-        
+
         this.client = TableClient
                 .newClient(GrpcTableRpc.ownTransport(transport))
                 .build();
-        
+
         this.retryCtx = SessionRetryContext.create(client).build();
-        
+
         initTable(transport.getDatabase() + "/" + TABLE_NAME);
     }
-    
+
     private void initTable(String tablePath) {
         try {
             Status dropResult = retryCtx.supplyStatus(session -> session.dropTable(tablePath))
@@ -67,6 +69,12 @@ public class YdbDataSource implements AutoCloseable, DataSource {
 
             retryCtx.supplyStatus(session -> session.createTable(tablePath, TABLE_DESCRIPTION))
                     .join().expect("can't create table " + tablePath);
+
+            String query = "\n"
+                    + "UPSERT INTO " + TABLE_NAME + "(name, date, uuid) VALUES ('Первый', CurrentUtcTimestamp(), '00000000-0000-0000-0000-000000000000');";
+            retryCtx.supplyResult(session -> session.executeDataQuery(query, TxControl.serializableRw()))
+                    .join().expect("can't read query result");
+
         } catch (UnexpectedResultException e) {
             log.error("can't init table", e);
             throw e;
@@ -122,13 +130,13 @@ public class YdbDataSource implements AutoCloseable, DataSource {
             // First SELECT from query
             ResultSetReader rs = result.getResultSet(0);
             List<User> list = new ArrayList<>();
-            
+
             while (rs.next()) {
                 String rowName = rs.getColumn("name").getUtf8();
                 Instant rowDate = rs.getColumn("date").getTimestamp();
                 list.add(new User(rowName, rowDate));
             }
-            
+
             return list;
         } catch (UnexpectedResultException e) {
             log.error("select record problem", e);
